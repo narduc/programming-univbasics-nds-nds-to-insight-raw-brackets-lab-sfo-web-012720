b17897b54b9bc094ed8eba59643f9995c4a232dd
Radniel Padron <radnielpadron@gmail.com>
2020-01-23 18:49:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,7 +29,7 @@ def directors_totals(nds)
   hash['Robert Zemeckis'] = 1273838385
   hash['Quentin Tarantino'] = 662738268
   hash['Martin Scorsese'] = 636812242
-  hash
+  hash['Francis Ford Coppola'] = 
   
   
   hash
